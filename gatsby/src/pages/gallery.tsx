f0542d495fb2a9877a4ad732e81f5350909ad31e
Jeff Holcomb <jeffh.84@gmail.com>
2024-03-04 19:26:37 -0800

    add more pictures to gallery

--- gatsby/src/pages/gallery.tsx
+++ gatsby/src/pages/gallery.tsx
@@ -3,12 +3,79 @@ import { useStaticQuery, graphql, HeadFC } from "gatsby";
 import { GalleryImageProps, QueryResult } from "../types";
 import Layout from "../Components/Layout";
 import GalleryImage from "../Components/GalleryImage";
+import styled from "styled-components";
+
+const ImageContainerStyles = styled.div`
+  width: 50%;
+  margin: auto;
+`;
 
 const GalleryPage: React.FC = () => {
   const images: GalleryImageProps[] = [
     {
       filename: "louise-desert-open.jpg",
       description: "Louise on a dry lake bed",
+    },
+    {
+      filename: "alive.jpg",
+      description: "Louise leading the Alive event",
+    },
+    {
+      filename: "charleston.jpg",
+      description: "Doing a yoga pose at Mt. Charleston",
+    },
+    {
+      filename: "danielle-louise.jpg",
+      description: "Louise and Danielle McCafferty at Mt. Charleston",
+    },
+    {
+      filename: "lake-bed-dani.jpg",
+      description: "Louise and Dani",
+    },
+    {
+      filename: "lake-bed-face.jpg",
+      description: "Louise at a dry lake bed",
+    },
+    {
+      filename: "lake-bed-side.jpg",
+      description: "Louise doing a side stretch on a dry lake bed",
+    },
+    {
+      filename: "lake-bed-1.jpg",
+      description: "Louise at a dry lake bed",
+    },
+    {
+      filename: "lake-bed-2.jpg",
+      description: "Louise at a dry lake bed",
+    },
+    {
+      filename: "lake-bed-3.jpg",
+      description: "Louise at a dry lake bed",
+    },
+    {
+      filename: "louise-desert-open.jpg",
+      description: "Louise doing a yoga pose in the desert",
+    },
+    {
+      filename: "red-rocks1.png",
+      description: "Louise at Red Rock State Park",
+    },
+    {
+      filename: "red-rocks2.png",
+      description: "Louise at Red Rock State Park",
+    },
+    {
+      filename: "red-rocks3.jpg",
+      description: "Louise at Red Rock State Park",
+    },
+    {
+      filename: "under-tree.jpeg",
+      description: "Louise under a tree",
+    },
+    {
+      filename: "vuori-event.jpeg",
+      description:
+        "Louise leading an event at the Vuori headquarters in Las Vegas",
     },
     // Add more images here
   ];
@@ -29,23 +96,25 @@ const GalleryPage: React.FC = () => {
   return (
     <Layout>
       <h2 className="title">Gallery</h2>
-      {images
-        .map(({ filename, description }) => {
-          const imageNode = data.allFile.nodes.find(
-            (node) => node.relativePath === filename
-          );
-          if (!imageNode) return null; // Skip rendering if the imageNode is not found
+      <ImageContainerStyles>
+        {images
+          .map(({ filename, description }) => {
+            const imageNode = data.allFile.nodes.find(
+              (node) => node.relativePath === filename
+            );
+            if (!imageNode) return null; // Skip rendering if the imageNode is not found
 
-          const imageData = imageNode.childImageSharp.gatsbyImageData;
-          return (
-            <GalleryImage
-              key={filename} // using filename as key since it's unique
-              imageData={imageData}
-              description={description}
-            />
-          );
-        })
-        .filter(Boolean)}{" "}
+            const imageData = imageNode.childImageSharp.gatsbyImageData;
+            return (
+              <GalleryImage
+                key={filename} // using filename as key since it's unique
+                imageData={imageData}
+                description={description}
+              />
+            );
+          })
+          .filter(Boolean)}{" "}
+      </ImageContainerStyles>
     </Layout>
   );
 };
